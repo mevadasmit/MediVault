b1c3472e54d1adce020c037af0bbd4ae44041f69
mevadsmit <mevadasmit2002@gmail.com>
2025-05-28 16:15:30 +0530

    fix the migration issue

--- inventory_manager/migrations/0006_inventory.py
+++ inventory_manager/migrations/0006_inventory.py
@@ -36,7 +36,7 @@ class Migration(migrations.Migration):
                     "category",
                     models.ForeignKey(
                         on_delete=django.db.models.deletion.CASCADE,
-                        to="main_admin.productcategory",
+                        to="main_admin.inventorycategory",
                     ),
                 ),
                 (
